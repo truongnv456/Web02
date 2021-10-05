d69968a3a2bd6222aeb7891e91262486557169a0
darknight1020 <haotocbac2000@gmail.com>
2021-10-06 00:22:16 +0700

    update again

--- Lab02/example.php
+++ Lab02/example.php
@@ -1,26 +1,31 @@
+<html>
+<head>
+    <title>Student Information</title>
+    <link rel="stylesheet" href="example.html">
+</head>
+<body>
+<h2> Student Information </h2>
 <?php
-if (isset($_POST['name'])) {
-   $name = $_POST['name'];
-   $university = $_POST['university'];
-   $class = $_POST['class'];
-   $email = $_POST['email'];
-   $telephone = $_POST['telephone'];
-   $address = $_POST['address'];
-   $control = $_POST['control'];
-   $hobby = $_POST['hobby'];
-
-   print("<br>Name: $name");
-   print("<br>Class: $class");
-   print("<br>University: $telephone");
-   print("<br>Email: $email");
-   print("<br>Telephone: $phone");
-
-    if ( !empty($hobby)) {
-    $N = count($hobby);
-    for ($i=0; $i < count($hobby); $i++) { 
-        $a = $i + 1;
-        echo $a.". ".$hobby[$i]."<br/>";
-    }
-    }
-    echo $register;
+$name = $_POST["name"];
+$gender = $_POST["gender"];
+$class = $_POST["class"];
+$university = $_POST["university"];
+$email= $_POST["email"];
+$telephone = $_POST["telephone"];
+$address = $_POST["address"];
+$hobbies = $_POST["hobby"];
+print ("<br> Name: $name");
+print ("<br> Gender: $gender");
+print ("<br> Class: $class");
+print ("<br> University: $university");
+print ("<br> Telephone: $telephone");
+print ("<br> Address: $address");
+print ("<br> Hobbies:");
+foreach($hobbies as $selected){
+    print ("$selected");
 }
+?>
+</form>
+</body>
+
+</html>
