ff708864b0d941be80454632b98b96c38e8315b5
darknight1020 <haotocbac2000@gmail.com>
2021-10-06 00:25:40 +0700

    update

--- Lab02/Form4Radio.php
+++ Lab02/Form4Radio.php
@@ -3,10 +3,9 @@
     <Title>Receving Input</Title>
 </head>
 <body>
-    <font size=5>Thank you: Got your input.</font>
     <?php
-        $email = $_POST("email");
-        $contact = $_POST("contact");
+        $email = $_POST["email"];
+        $contact = $_POST["contact"];
         print("<br>Your email address is $email");
         print("<br>Contact preference is $contact");
     ?>
