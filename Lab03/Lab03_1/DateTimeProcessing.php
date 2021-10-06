d72965cd52d9114f5b4033b802bb0b17079eecf2
darknight1020 <haotocbac2000@gmail.com>
2021-10-06 10:14:17 +0700

    update

--- Lab03/Lab03_1/DateTimeProcessing.php
+++ Lab03/Lab03_1/DateTimeProcessing.php
@@ -115,19 +115,10 @@ if (array_key_exists("name", $_GET)) {
     }
     print("<br>In 12 hours the time and date is $hour:$min:$sec $status ,$day/$month/$year ");
     switch ($month) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
+        case 1:case 3:case 5:case 7:case 8:case 10:case 12:
             print("<br>This month has 31 days!");
             break;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
+        case 4:case 6:case 9:case 11:
             print("<br>This month has 30 days!");
             break;
         case 2:
